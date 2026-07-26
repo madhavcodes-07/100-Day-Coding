b4976bbde84a76471083f9570f92e2cf5ec062c3
Madhav Semwal <Madhav.12637@stu.upes.ac.in>
2026-07-26 12:34:58 +0530

    Add files via upload

---
+++ polynomial_linkedlist.cpp
@@ -0,0 +1,97 @@
+//Problem: Polynomial Using Linked List - Implement using linked list with dynamic memory allocation.
+
+//Input:
+// - First line: integer n (number of terms)
+// - Next n lines: two integers (coefficient and exponent)
+
+//Output:
+// - Print polynomial in standard form, e.g., 10x^4 + 20x^3 + 30x^2 + 40x + 50
+
+//Example:
+//Input:
+//5
+//10 4
+//20 3
+//30 2
+//40 1
+//50 0
+
+//Output:
+//10x^4 + 20x^3 + 30x^2 + 40x + 50
+
+//Explanation:
+//Each node stores coefficient and exponent. Traverse nodes to print polynomial in decreasing exponent order.                    
+
+
+
+
+//solution:
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// Node structure for each term
+struct Node {
+    int coeff;
+    int exp;
+    struct Node *next;
+};
+
+// Create a new node
+struct Node* createNode(int coeff, int exp) {
+    struct Node *newNode = (struct Node*) malloc(sizeof(struct Node));
+    newNode->coeff = coeff;
+    newNode->exp = exp;
+    newNode->next = NULL;
+    return newNode;
+}
+
+// Insert node at the end of the list
+struct Node* insertEnd(struct Node *head, int coeff, int exp) {
+    struct Node *newNode = createNode(coeff, exp);
+    if (head == NULL) return newNode;
+
+    struct Node *temp = head;
+    while (temp->next != NULL)
+        temp = temp->next;
+    temp->next = newNode;
+    return head;
+}
+
+// Print the polynomial
+void printPolynomial(struct Node *head) {
+    struct Node *temp = head;
+    while (temp != NULL) {
+        if (temp->exp == 0)
+            printf("%d", temp->coeff);
+        else if (temp->exp == 1)
+            printf("%dx", temp->coeff);
+        else
+            printf("%dx^%d", temp->coeff, temp->exp);
+
+        if (temp->next != NULL)
+            printf(" + ");
+
+        temp = temp->next;
+    }
+    printf("\n");
+}
+
+int main() {
+    int n, coeff, exp;
+    struct Node *head = NULL;
+
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d %d", &coeff, &exp);
+        head = insertEnd(head, coeff, exp);
+    }
+
+    printPolynomial(head);
+
+    return 0;
+}
+
