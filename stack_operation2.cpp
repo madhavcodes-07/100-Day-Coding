1507d2db33e84e0d116073cce8a5fce0cbbf9112
Madhav Semwal <Madhav.12637@stu.upes.ac.in>
2026-07-28 11:52:15 +0530

    Add files via upload

---
+++ stack_operation2.cpp
@@ -0,0 +1,56 @@
+//Problem: Implement push and pop operations on a stack and verify stack operations.
+
+//Input:
+//- First line: integer n
+//- Second line: n integers to push
+//- Third line: integer m (number of pops)
+
+//Output:
+//- Print remaining stack elements from top to bottom
+
+//Example:
+//Input:
+//5
+//10 20 30 40 50
+//2
+
+//Output:
+//30 20 10
+
+
+
+//solution:
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    
+    stack<int> st;
+    for (int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+        st.push(x);
+    }
+    
+    int m;
+    cin >> m;
+    for (int i = 0; i < m; i++) {
+        if (!st.empty()) st.pop();
+    }
+    
+    bool first = true;
+    while (!st.empty()) {
+        if (!first) cout << " ";
+        cout << st.top();
+        st.pop();
+        first = false;
+    }
+    cout << endl;
+    
+    return 0;
+}
