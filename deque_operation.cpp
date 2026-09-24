5b85c940a67f4452065e6be22d51ea7bd54089c7
Madhav Semwal <Madhav.12637@stu.upes.ac.in>
2026-09-24 20:03:45 +0530

    Add files via upload

--- deque_operation.cpp
+++ deque_operation.cpp
@@ -1,5 +1,6 @@
-//A Deque is a linear data structure that allows insertion and deletion of elements from both the front and the rear.
-// It provides more flexibility than a standard queue or stack.
+//Problem: Deque (Double-Ended Queue)
+
+//A Deque is a linear data structure that allows insertion and deletion of elements from both the front and the rear. It provides more flexibility than a standard queue or stack.
 
 //Common Operations:
 //1. push_front(value): Insert an element at the front of the deque.
@@ -12,8 +13,8 @@
 //8. size(): Return the number of elements in the deque.
 
 //Additional Operations:
-// - clear(): Remove all elements from the deque.
-// - erase(): Remove one or more elements from the deque.
+//- clear(): Remove all elements from the deque.
+//- erase(): Remove one or more elements from the deque.
 //- swap(): Swap contents of two deques.
 //- emplace_front(): Insert an element at the front without copying.
 //- emplace_back(): Insert an element at the rear without copying.
@@ -35,6 +36,8 @@
 
 
 
+
+
 //solution:
 
 
@@ -43,58 +46,50 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    deque<int> dq;
-    int q;
-    cin >> q; // number of operations
-
-    while (q--) {
-        string op;
-        cin >> op;
-
-        if (op == "push_front") {
-            int val; cin >> val;
-            dq.push_front(val);
-        }
-        else if (op == "push_back") {
-            int val; cin >> val;
-            dq.push_back(val);
-        }
-        else if (op == "pop_front") {
-            if (!dq.empty()) dq.pop_front();
-        }
-        else if (op == "pop_back") {
-            if (!dq.empty()) dq.pop_back();
-        }
-        else if (op == "front") {
-            if (!dq.empty()) cout << dq.front() << "\n";
-            else cout << "Deque is empty\n";
-        }
-        else if (op == "back") {
-            if (!dq.empty()) cout << dq.back() << "\n";
-            else cout << "Deque is empty\n";
-        }
-        else if (op == "empty") {
-            cout << (dq.empty() ? "true" : "false") << "\n";
-        }
-        else if (op == "size") {
-            cout << dq.size() << "\n";
-        }
-        else if (op == "clear") {
-            dq.clear();
-        }
-        else if (op == "reverse") {
-            reverse(dq.begin(), dq.end());
-        }
-        else if (op == "sort") {
-            sort(dq.begin(), dq.end());
-        }
-    }
-
-    // Print final state of the deque
-    cout << "Final Deque: ";
+void printDeque(const deque<int>& dq) {
     for (int x : dq) cout << x << " ";
     cout << "\n";
+}
 
+int main() {
+    deque<int> dq;
+
+    // Insertion at both ends
+    dq.push_back(10);        // [10]
+    dq.push_back(20);        // [10 20]
+    dq.push_front(5);        // [5 10 20]
+    dq.emplace_front(1);     // [1 5 10 20]
+    dq.emplace_back(30);     // [1 5 10 20 30]
+
+    // Access
+    cout << "front: " << dq.front() << "\n";   // 1
+    cout << "back: "  << dq.back()  << "\n";   // 30
+    cout << "size: "  << dq.size()  << "\n";   // 5
+    cout << "empty: " << dq.empty() << "\n";   // 0
+
+    // Removal at both ends
+    dq.pop_front();          // [5 10 20 30]
+    dq.pop_back();           // [5 10 20]
+
+    // Erase a single element / a range
+    dq.erase(dq.begin() + 1);                  // [5 20]
+    dq.assign({4, 2, 9, 7, 1});                // [4 2 9 7 1]
+    dq.erase(dq.begin(), dq.begin() + 2);      // [9 7 1]
+
+    // Algorithms (not member functions)
+    reverse(dq.begin(), dq.end());             // [1 7 9]
+    dq.push_back(3);                           // [1 7 9 3]
+    sort(dq.begin(), dq.end());                // [1 3 7 9]
+
+    // Resize and swap
+    dq.resize(6);                              // [1 3 7 9 0 0]
+    deque<int> other = {100, 200};
+    dq.swap(other);                            // dq = [100 200], other = [1 3 7 9 0 0]
+
+    printDeque(dq);
+    printDeque(other);
+
+    dq.clear();
+    cout << "size after clear: " << dq.size() << "\n";   // 0
     return 0;
 }
